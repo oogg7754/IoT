55dbde2f0039590ceaf17969bb49476e422d8692
unknown <oogg7754@gmail.com>
2020-07-13 19:03:16 +0900

    0713-1

---
+++ src/layout/styles.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    position: relative;
+    .footer {
+        position: absolute:
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        background: #f2f3f4;
+    }
+    .container {
+        margin-bottom: 30px;
+    }
+`;
+
+export default Wrapper;
